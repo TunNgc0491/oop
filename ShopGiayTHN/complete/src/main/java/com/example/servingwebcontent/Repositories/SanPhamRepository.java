765f20bd4df1f9958b0b3ac0164e3ef830fcd0c1
tunngc0941 <24100498@st.phenikaa-uni.edu.vn>
2025-08-11 09:11:03 +0000

    dlt

--- ShopGiayTHN/complete/src/main/java/com/example/servingwebcontent/Repositories/SanPhamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.servingwebcontent.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.servingwebcontent.model.SanPham;
-
-public interface SanPhamRepository extends JpaRepository<SanPham, Long> {
-}
